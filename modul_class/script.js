da2970a80434ab996bf6f569bcf6e7a590a8a4b1
Alex <sanichen350@gmail.com>
2021-06-05 10:18:28 +0300

    class continue

--- modul_class/script.js
+++ modul_class/script.js
@@ -1,3 +1,37 @@
-'use strict';
+// 'use strict';
 
+function button(){
+    return 'button';
+}
+
+class Slider{
+    constructor(width,height,count){
+      this.width = width;
+      this.height = height;
+      this.count = count;
+    }
+    nexSlider(){
+        console.log('Moving forward');
+    }
+    prewSlider(){
+        console.log('Moving back');
+    }
+    whoAmI(){ 
+        console.log(this.width, this.height, this.count);
+    }
+}
+
+//------------------new class AutoSlider ------------------
+
+class AutoSlider extends Slider{
+    constructor(width,height,count,auto){
+    super(width,height,count);  
+    this.auto = auto;  
+    }
+    play(){
+        console.log(`Atoplay: ${this.auto}`);
+    }
+
+}
+export{button,Slider, AutoSlider};
 
