9a5b4e595f82f3f30f69e9a3be9a573d8cea82dd
Alex <sanichen350@gmail.com>
2021-06-27 07:27:33 +0300

    react_lesson_1

--- learn_react_1/src/Nav.js
+++ learn_react_1/src/Nav.js
@@ -6,23 +6,45 @@ class Nav extends React.Component {
         this.state = {
             title:'Navigation',
             subtitle:'Main menu',
-            show:'show'
+            show:'show',
+            text:'',
+            age: 0
         }
     }
     showNaw = () => {
        this.setState({show: "hide"});
     }
 
+    myInput = (e) =>{
+        this.setState({[e.target.name]:e.target.value});
+
+    }
+    submitForm =(e) => {
+        e.preventDefault();
+    }
   render() {
-   let nav3 = this.props.nav2;
+      let a = ''
+      if(this.state.text){
+        a = <p>Your text: {this.state.text}</p>
+      }
+      else{
+        a = '';
+     }
+     let nav3 = this.props.nav2;
     return (
          <nav>
              <button type="button" onClick={this.showNaw}>Show menu</button>
              <h1>{this.state.title}</h1>
              <h2>{this.state.subtitle}</h2>
              <p>{this.state.show}</p>
-
-             <ul>
+             <form onSubmit={this.submitForm}>
+               <input onChange={this.myInput} name="text"/>
+               <input onChange={this.myInput} type="number" name="age"/>
+               <input type="submit"/>
+           <p>Age:{this.state.age}</p>
+            </form>
+            <p>{a}</p>
+            <ul>
                {Object.keys(nav3).map(elem => {
                   return <li><a href="{nav3[elem]}">{elem}</a></li>;
                 })} 
