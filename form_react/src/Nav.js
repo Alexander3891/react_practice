8fbbd48f9077a5e0a8ad03974e3f01f1c46a7cf5
Alex <sanichen350@gmail.com>
2021-07-01 09:49:17 +0300

    form_value_table

--- form_react/src/Nav.js
+++ form_react/src/Nav.js
@@ -13,8 +13,25 @@ class Nav extends React.Component {
             subtitle:'Main menu',
             show:'show',
             text:'',
-            age: 0
+            age: 0,
+            value:'0',
+            table:{
+            border:'1px solid black',
+            margin: 'auto',
+            backgroundColor:'red'
+            },
+            text2:'',
+            ege2: 0,
+           randge:0,
+           textarea:'',
+           select:''
         }
+  this.text_form = this.text_form.bind(this);
+  this.age_form = this.age_form.bind(this);
+  this.range_form = this.range_form.bind(this);
+  this.submitForm2 = this.submitForm2.bind(this);
+
+
     }
     showNaw = () => {
        this.setState({show: "hide"});
@@ -27,6 +44,29 @@ class Nav extends React.Component {
     submitForm =(e) => {
         e.preventDefault();
     }
+
+// ======================
+text_form(e){
+  this.setState({[e.target.name]:e.target.value});
+}
+age_form(){
+ 
+}
+range_form(e){
+  this.setState({value: e.target.value})
+}
+textarea_form(){
+  
+}
+select_form(){
+
+}
+submitForm2(e){
+  e.preventDefault();
+
+}
+//==========================
+
   render() {
       let a = ''
       if(this.state.text){
@@ -74,6 +114,45 @@ class Nav extends React.Component {
                   return <li><a href="{nav3[elem]}">{elem}</a></li>;
                 })} 
             </ul>
+
+            {/* //==================================================== */}
+            <form onSubmit={this.submitForm2}>
+               <input onChange={this.text_form} name="text2"/>
+               <input onChange={this.text_form}_form type="number" name="age2"/>
+               <input onChange={this.range_form} type="range" min="1" max="100" value={this.state.value} name="range"/>
+               <textarea onChange={this.text_form}  name="textarea"/>
+               <select onChange={this.text_form} name="select">
+                <option >Пункт 1</option>
+                <option >Пункт 2</option> 
+                <option >Пункт 3</option> 
+                <option>Пункт 4</option> 
+         </select>
+              <p><input type="submit"/></p>
+              <table style={this.state.table}>
+               <tr>
+               <td>Text2:</td>
+               <td>{this.state.text2}</td>
+              </tr>
+              <tr>
+               <td>Age2:</td>
+               <td>{this.state.age2}</td>
+              </tr>
+              <tr>
+               <td>range:</td>
+               <td>{this.state.value}</td>
+              </tr>
+              <tr>
+               <td>textarea:</td>
+               <td>{this.state.textarea}</td>
+              </tr>
+              <tr>
+               <td>select:</td>
+               <td>{this.state.select}</td>
+              </tr>
+              </table>
+
+            </form>
+        
         </nav>
     </div>
     );   
