4e5ff99150186646b595c1fc232e9062f4dad931
Alex <sanichen350@gmail.com>
2021-07-05 11:52:29 +0300

    create new api

--- exchange/src/Rate/Rate.js
+++ exchange/src/Rate/Rate.js
@@ -1,41 +1,34 @@
 import React from 'react';
 import './Rate.css';
+import Calc from '../Calc/Calc'
 
 
 class Rate extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            'date': new Date().toLocaleString(),
-            'USD': '',
-            'EUR': '',
-            'RUR': '',
-            'USD_buy': '',
-            'EUR_buy': '',
-            'RUR_buy': '',
-            'USD_sale': '',
-            'EUR_sale': '',
-            'RUR_sale': '',
-        
+        'data': '',      
+        'currencyRate': {}
         }
+        this.currency = ['USD', 'RUB', 'CAD', 'PHP']
         this.getRate();
         }
+
     getRate = () => {
-        fetch('https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5')
+        fetch('http://api.exchangeratesapi.io/v1/latest?access_key=e99be8401a607f457037e674286f1c89')
             .then(data => {
                 return data.json();
             })
             .then(data => {
-                this.setState({ USD: data[0].ccy })
-                this.setState({EUR: data[1].ccy})
-                this.setState({RUR: data[2].ccy})
-                this.setState({USD_buy: data[0].buy })
-                this.setState({EUR_buy: data[1].buy})
-                this.setState({RUR_buy: data[2].buy })
-                this.setState({USD_sale: data[0].sale })
-                this.setState({EUR_sale: data[1].sale})
-                this.setState({RUR_sale: data[2].sale})
-            })
+            this.setState({date: data.date});
+          let result = {};
+          for(let i = 0; i < this.currency.length; i++){
+          result[this.currency[i]] = data.rates[this.currency[i]]
+         }
+         this.setState({currencyRate: result});
+
+      });  
+     
             
     }
     render() {
@@ -44,22 +37,18 @@ class Rate extends React.Component {
                 <h3>Курс валют на {this.state.date}</h3>
                 
             <div className="flex-container">
-                <div className=" block flex-item">
-                        <div className="currency-name">{this.state.USD}</div>
-                        <div className="currency-in"><span className="currency-text">Покупка: </span>{Number(this.state.USD_buy).toFixed(2)} грн</div>
-                        <div className="currency-out"><span className="currency-text">Продажа: </span>{Number(this.state.USD_sale).toFixed(2)} грн</div>
+                {Object.keys(this.state.currencyRate).map( (keyName,i) =>
+                (
+                         <div className=" block flex-item" key={keyName}>
+                        <div className="currency-name">{keyName}</div>
+                        <div className="currency-in">{Number(this.state.currencyRate[keyName]).toFixed(2)}</div>
+                        <p>* Можно купить за 1 EUR</p>            
             </div>
-            <div className=" block flex-item">
-                <div className="currency-name">{this.state.EUR}</div>
-                <div className="currency-in"><span className="currency-text">Покупка: </span> {Number(this.state.EUR_buy).toFixed(2)} грн</div>
-                <div className="currency-out"><span className="currency-text">Продажа: </span>{Number(this.state.EUR_sale).toFixed(2)} грн</div>
-            </div>
-            <div className=" block flex-item">
-                <div className="currency-name">{this.state.RUR}</div>
-                <div className="currency-in"><span className="currency-text">Покупка: </span>{Number(this.state.RUR_buy).toFixed(2)} грн</div>
-                <div className="currency-out"><span className="currency-text">Продажа: </span>{Number(this.state.RUR_buy).toFixed(2)} грн</div>
-            </div>
+                )
+                )}
+                
                 </div>
+                <Calc us={this.state.USD_buy}/>
                 </div>
         );
     }
