3428bed227731a3c0386b3aaeb9682910e037175
Alex <sanichen350@gmail.com>
2021-07-08 17:40:41 +0300

    unit3_finish

---
+++ unit_2_practic/src/Footer/Footer.js
@@ -0,0 +1,10 @@
+function Footer(props) {
+    return (
+        <footer>
+	<h3>{props.siteData.site_name}</h3>
+	сюда импортируем компонент nav
+</footer>
+
+        );
+}
+export default Footer;
---
+++ unit_3/src/Users.js
@@ -0,0 +1,11 @@
+function Users() {
+    return (
+        <>
+            <h1>Users</h1>
+            <li><a href="/users/ivanov">Ivanov</a></li>
+            <li><a href="/users/petrov">Petrov</a></li>
+
+        </>
+    );
+}
+export default Users;
