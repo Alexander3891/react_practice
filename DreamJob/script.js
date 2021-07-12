2d3264e10d6afa0556388dbf4e03daadbd8bdbde
Alex <sanichen350@gmail.com>
2021-07-12 18:14:05 +0300

    hover price at cart

--- DreamJob/script.js
+++ DreamJob/script.js
@@ -1,4 +1,8 @@
-var slideIndex = 1;
+"use strict";
+
+
+    //========== slider 1 ==================
+let slideIndex = 1;
 showSlides(slideIndex);
 
 // Вперед/назад элементы управления
@@ -12,9 +16,9 @@ function currentSlide(n) {
 }
 
 function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
@@ -25,4 +29,44 @@ function showSlides(n) {
   }
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
+}
+
+ //========== slider 2 ==================
+let slideIndex2 = 1;
+showSlides2(slideIndex2);
+
+// Вперед/назад элементы управления
+function plusSlides2(n) {
+  showSlides2(slideIndex2 += n);
+}
+
+// Элементы управления миниатюрами изображений
+function currentSlide2(n) {
+  showSlides2(slideIndex2 = n);
+}
+
+function showSlides2(n) {
+  let j;
+  let slides2 = document.getElementsByClassName("mySlides2");
+  let dots2 = document.getElementsByClassName("dot2");
+  if (n > slides2.length) {slideIndex2 = 1}
+  if (n < 1) {slideIndex2 = slides2.length}
+  for (j = 0; j < slides2.length; j++) {
+      slides2[j].style.display = "none";
+  }
+  for (j = 0; j < dots2.length; j++) {
+      dots2[j].className = dots2[j].className.replace(" active", "");
+  }
+  slides2[slideIndex2-1].style.display = "block";
+  dots2[slideIndex2-1].className += " active";
+}
+//  ============= hover price at cart ================
+  let a = document.querySelector('.main_cart_price_price_2');
+  //вешаем на него события
+a.onmouseover = function (e) {
+    document.querySelector('.main_cart_price_price_2_less').style.display = 'block';
+}
+
+a.onmouseout = function (e) {
+    document.querySelector('.main_cart_price_price_2_less').style.display = 'none';
 }
